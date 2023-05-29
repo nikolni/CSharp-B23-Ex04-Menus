73f4f989e0e433d37e936d4462370cb8bdf84ff1
nikolni <nikolni@mta.ac.il>
2023-05-29 18:01:34 +0300

    Add project files.

---
+++ Ex04.Menus. Interfaces/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace Ex04.Menus.Interfaces
+{
+    public interface SelectedItemListener
+    {
+        void ImplementAnItem(MenuItem i_MenuItem);
+    }
+
+    internal class MainMenu : SelectedItemListener
+    {
+
+
+        public void ImplementAnItem(MenuItem i_MenuItem)
+        {
+            i_MenuItem.ActivateItem();
+        }
+    }
+}
---
+++ Ex04.Menus. Interfaces/MenuItem.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace Ex04.Menus.Interfaces
+{
+
+    public interface MenuItem
+    {
+        void ActivateItem();
+        void NotifySelectedItemListener();
+    }
+
+
+    public class Submenu : MenuItem
+    {
+        SelectedItemListener selectedItemListener = new SelectedItemListener();
+
+
+        LinkedList<MenuItem> m_ItemsList;
+
+        public void NotifySelectedItemListener()
+        {
+
+        }
+
+        public void ActivateItem()
+        {
+            PrintSubMenu();
+
+
+        }
+    }
+
+
+    public class FinalItem : MenuItem
+    {
+        public void NotifySelectedItemListener()
+        {
+
+        }
+
+
+        public void ActivateItem()
+        {
+
+        }
+    }
+
+}

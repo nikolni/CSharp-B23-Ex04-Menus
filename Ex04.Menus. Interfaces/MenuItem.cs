14c672d069c35cb8925fa220e2c74f0646e7b982
nikolni <nikolni@mta.ac.il>
2023-05-31 15:31:51 +0300

    last changes for interfaces project

--- Ex04.Menus. Interfaces/MenuItem.cs
+++ Ex04.Menus. Interfaces/MenuItem.cs
@@ -1,48 +1,118 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Ex04.Menus.Interfaces
 {
-
-    public interface MenuItem
+    public interface IMenuItemNotifier
     {
         void ActivateItem();
-        void NotifySelectedItemListener();
+        //void NotifySelectedItemListener();
     }
 
 
-    public class Submenu : MenuItem
+    public class MenuItem : IMenuItemNotifier
     {
-        SelectedItemListener selectedItemListener = new SelectedItemListener();
+       // SelectedItemListener selectedItemListener = new SelectedItemListener();
+        public string m_Title; 
+        public List<MenuItem> m_SubItems; 
 
+        //public bool IsActionItem { get; set; }
+        public IMenuItemNotifier m_Notifier; 
 
-        LinkedList<MenuItem> m_ItemsList;
-
-        public void NotifySelectedItemListener()
+        //propetties
+        public string Title
         {
+            get
+            {
+                return m_Title;
+            }
+            set
+            {
+                m_Title = value;
+            }
+                
+        }
 
+        public List<MenuItem> SubItems
+        {
+            get
+            {
+                return m_SubItems;
+            }
+            set
+            {
+                m_SubItems = value;
+            }
+        }
+
+        public IMenuItemNotifier Notifier
+        {
+            get
+            {
+                return m_Notifier;
+            }
+            set
+            {
+                m_Notifier = value;
+            }
+        }
+
+        public MenuItem(string i_Title)
+        {
+            m_Title = i_Title;
+            m_SubItems = new List<MenuItem>();
+            //IsActionItem = false;
+        }
+
+        public MenuItem(string i_Title, IMenuItemNotifier i_Notifier)
+        {
+            m_Title = i_Title;
+            m_SubItems = new List<MenuItem>();
+            //IsActionItem = true;
+            m_Notifier = i_Notifier;
         }
 
         public void ActivateItem()
         {
-            PrintSubMenu();
-
-
+            m_Notifier.ActivateItem();
         }
     }
 
 
-    public class FinalItem : MenuItem
+    // Notifier implementations for the action items
+    public class TimeShowNotifier : IMenuItemNotifier
     {
-        public void NotifySelectedItemListener()
-        {
-
-        }
-
-
         public void ActivateItem()
         {
-
+            Console.WriteLine($"The Hour is: {DateTime.Now.ToString("HH:mm:ss")}");
         }
     }
 
+    public class DateShowNotifier : IMenuItemNotifier
+    {
+        public void ActivateItem()
+        {
+            Console.WriteLine($"The Date is: {DateTime.Today.ToString("yyyy-MM-dd")}");
+        }
+    }
+
+    public class ShowVersionNotifier : IMenuItemNotifier
+    {
+        public void ActivateItem()
+        {
+            Console.WriteLine("Version: 23.2.4.9805");
+        }
+    }
+
+    public class CountSpacesNotifier : IMenuItemNotifier
+    {
+        public void ActivateItem()
+        {
+            Console.Write("Enter a sentence: ");
+            string sentence = Console.ReadLine();
+            int spaceCount = sentence.Count(c => c == ' ');
+            Console.WriteLine($"Number of spaces in the sentence: {spaceCount}");
+        }
+    }
 }
--- Ex04.Menus.Test/Program.cs
+++ Ex04.Menus.Test/Program.cs
@@ -1,7 +1,38 @@
-﻿namespace Ex04.Menus.Test
-{
-    internal class Program
-    {
+﻿using Ex04.Menus.Interfaces;
 
+namespace Ex04.Menus.Test
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            MainMenu mainMenuForInterfaces = new MainMenu();
+            //MainMenu mainMenuForDelegates = new MainMenu();
+
+            // Build the first menu for interface
+            MenuItem dateTimeMenuItem = new MenuItem("Show Date/Time");   //first level
+            MenuItem dateShowMenuItem = new MenuItem("Show Date", new DateShowNotifier());  //second level
+            MenuItem timeShowMenuItem = new MenuItem("Show Time", new TimeShowNotifier());  //second level
+            timeDateMenuItem.m_SubItems.Add(dateShowMenuItem);
+            timeDateMenuItem.m_SubItems.Add(timeShowMenuItem);
+
+            MenuItem versionSpacesMenuItem = new MenuItem("Version and Spaces");   //first level
+            MenuItem showVersionMenuItem = new MenuItem("Show Version", new ShowVersionNotifier());  //second level
+            MenuItem countSpacesMenuItem = new MenuItem("Count Spaces", new CountSpacesNotifier());  //second level
+            versionSpacesMenuItem.m_SubItems.Add(showVersionMenuItem);
+            versionSpacesMenuItem.m_SubItems.Add(countSpacesMenuItem);
+
+            mainMenuForInterfaces.AddMenuItem(dateTimeMenuItem);
+            mainMenuForInterfaces.AddMenuItem(versionSpacesMenuItem);
+
+            // Display the first menu
+            mainMenuForInterfaces.Show();
+
+
+            // Build the first menu for delegates
+
+            // Display the first menu
+            //mainMenuForDelegates.Show();
+        }
     }
 }
